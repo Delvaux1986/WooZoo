b06d31832fbaafe18907c173ef4ce107414da6de
unknown <delvaux.robby@gmail.com>
2021-05-07 20:37:09 +0200

    heroku10

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -18,7 +18,7 @@
                     <li>
                         <hr class="dropdown-divider">
                     </li>
-                    <li><a class="dropdown-item" :href="route('logout')">Logout</a></li>
+                    <li><a class="dropdown-item" :href="route('Mylogout')">Logout</a></li>
                 </ul>
             </li>
         </ol>
